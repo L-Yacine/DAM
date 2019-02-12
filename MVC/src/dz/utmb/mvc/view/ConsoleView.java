21fb80c8d4d4e6f624fa28d7ae2c8fe8b885fb89
Yacine <labdaoui.yacine@outlook.com>
2019-02-12 20:41:53 +0100

    Finished View package. Controller package is next to come

---
+++ MVC/src/dz/utmb/mvc/view/ConsoleView.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dz.utmb.mvc.view;
+import java.util.List;
+import dz.utmb.mvc.model.Employee;
+public class ConsoleView implements View {
+    @Override
+    public void showEnrolledEmployees(List<Employee> enrolledEmployees)
+    {
+    }
+}
+
